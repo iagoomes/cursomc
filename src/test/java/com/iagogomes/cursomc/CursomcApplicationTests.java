64d105b4b66f0e8957d70471c7e5d87861844ce5
iagoomes <iagoomes@outlook.com>
2021-05-30 09:17:31 -0300

    Projeto criado

---
+++ src/test/java/com/iagogomes/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagogomes.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
